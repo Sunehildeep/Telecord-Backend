24334dcb678db9e8e398705ca2792165b79bd4b9
GarvChhokra <garvchhokra@gmail.com>
2024-03-11 02:41:22 -0400

    removed print statements

--- chat/main.py
+++ chat/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, session
+from flask import Flask, session, request
 from flask_socketio import SocketIO, send, emit, join_room, leave_room
 import os
 from dotenv import load_dotenv
@@ -21,15 +21,14 @@ def join_community(data):
     user_id = data['user_id']
     community_id = data['community_id']
     if user_id and community_id:
-        join_room(community_id)
+        # TODO : Add user to community
         send(str(user_id) + ' has entered the room.', to=community_id)
-        print('Client joined community: ' + str(data['community_id']))
 
 @socketio.on('leave_community')
 def on_leave(data):
     user_id = data['user_id']
     community_id = data['community_id']
-    leave_room(community_id)
+    # TODO : Remove user from community
     send(user_id + ' has left the room.', to=community_id)
 
 @socketio.on('message_send')
@@ -37,8 +36,8 @@ def handle_message(data):
     user_id = data['user_id']
     community_id = data['community_id']
     message = data['message']
-    print('received message: ' + str(data))
     if user_id and community_id and message:
+        # TODO : Save message to database
         emit('new_msg', {'user_id': user_id, 'community_id': community_id, 'message': message}, broadcast=True)
 
 if __name__ == '__main__':
