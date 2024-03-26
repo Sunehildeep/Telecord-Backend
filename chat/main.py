9d239ab86dcc293a45b55f317b553118c95f5533
Sunehildeep <sunehildeepsinghsandhu123@gmail.com>
2024-03-26 19:21:29 -0400

    chat funtionality on aws + socket

--- chat/main.py
+++ chat/main.py
@@ -10,35 +10,36 @@ FE_URL = os.getenv("FE_URL")
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = secretKey
-socketio = SocketIO(app, cors_allowed_origins=FE_URL , cors_allowed_methods=["GET", "POST"], cors_allowed_headers="*")
+socketio = SocketIO(app, cors_allowed_origins=FE_URL, cors_allowed_methods=[
+                    "GET", "POST"], cors_allowed_headers="*")
+
 
 @socketio.on('connect')
 def test_connect():
     print('Client connected')
 
+
 @socketio.on('join_community')
-def join_community(data):
-    user_id = data['user_id']
-    community_id = data['community_id']
-    if user_id and community_id:
-        # TODO : Add user to community
-        send(str(user_id) + ' has entered the room.', to=community_id)
+def join_community(CommunityId):
+    print('User joined community', str(CommunityId))
+    join_room(str(CommunityId))
+
 
 @socketio.on('leave_community')
-def on_leave(data):
-    user_id = data['user_id']
-    community_id = data['community_id']
-    # TODO : Remove user from community
-    send(user_id + ' has left the room.', to=community_id)
+def on_leave(CommunityId):
+    print('User left community', str(CommunityId))
+    leave_room(str(CommunityId))
+
 
 @socketio.on('message_send')
 def handle_message(data):
-    user_id = data['user_id']
-    community_id = data['community_id']
-    message = data['message']
-    if user_id and community_id and message:
-        # TODO : Save message to database
-        emit('new_msg', {'user_id': user_id, 'community_id': community_id, 'message': message}, broadcast=True)
+    print('received message: ' + data['Username'])
+    username = data['Username']
+    community_id = data['CommunityId']
+    message = data['Message']
+    if username and community_id and message:
+        emit('new_msg', data, room=community_id)
+
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
